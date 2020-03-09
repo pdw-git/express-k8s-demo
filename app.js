092627515046e1102a2fc83f3e0ee5cb82613635
pdw-git <bose.lane@btinternet.com>
2020-03-09 15:43:40 +0000

    Moved from using Jade to using Pug for view rendering. Updated the package.json file to use latest versions of dependencies.

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname,'app_server', 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.locals.pretty = true; //format the HTML so it is human readable
 
 app.use(logger('dev'));
