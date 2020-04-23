80941bc9a2a46f2d530bc169a3c4445b4b48e6f3
whitep <bose.lane@btinternet.com>
2020-04-23 19:53:02 +0100

    Added body and statu checks

--- test/testAPI/deleteConfigGoodStatus.js
+++ test/testAPI/deleteConfigGoodStatus.js
@@ -15,8 +15,16 @@ const apiCall = '/config/';
 const testName = 'deleteConfigGoodStatus';
 
 //============================================================================================
-// POST /api/config/:configid good status
+// DELETE /api/config/:configid good status
 //============================================================================================
+
+//open a connection to the mongo db
+//create a new config object in the data base
+//get the id of this new object
+//save the id and update the expected body with the id information
+//delete the config
+//valdiate the body and status match expectations
+
 
 module.exports.deleteConfigGoodStatus={
 
@@ -46,23 +54,31 @@ module.exports.deleteConfigGoodStatus={
     tests:{
         before : (testData, done)=>{
 
-
+            //we need to open a connection to the data base. Remember the tests run outside of the the main applicaiton
             mongoose.connect(process.env.MONGO_URI+process.env.MONGO_DB_NAME,
                 {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false}
                 ).catch((reason)=>{testFunction.handleError(new Error(reason), done)});
 
+            //check that there is currently only one config document in the database
             request(testData.requestOptions.getConfig, (err, response, body)=> {
 
                 if (Array.isArray(body) && (body.length === 1)) {
 
+                    //create a new config document
                     let configModel = mongo.getMongoObject(appConfig.mongo.configObjectName, schema.getSchema());
                     let configObject = new configModel(configDB.getConfig());
 
+                    //save the new document
                     // noinspection JSIgnoredPromiseFromCall supress warning on promise not being caught. Adding .catch caused functional issues.
                     configObject.save((err, doc)=>{
 
+                        //save id of new doc, update the expected body data and update the url for the delete request options
                         testData.setupData.data._id = doc._id;
+                        testData.expectedBody.msg = testData.expectedBody.msg+doc._id;
+                        testData.requestOptions.deleteConfig.url = testData.requestOptions.deleteConfig.url+testData.setupData.data._id;
 
+
+                        //do the actual test
                         err ? testFunction.handleError(err, done) :
                         testData.tests.testAction(testData, done);
 
@@ -78,11 +94,20 @@ module.exports.deleteConfigGoodStatus={
         },
         testAction: (testData, done)=>{
 
-            testData.requestOptions.deleteConfig.url = testData.requestOptions.deleteConfig.url+testData.setupData.data._id;
+            //request the config delete
+            request(testData.requestOptions.deleteConfig, (err, response, body)=>{
 
-            request(testData.requestOptions.deleteConfig, (err)=>{
+                //set up the assertion data
+                testData.setupData.assert = [
+                    {assertion: testData.tests.statusAssertion, actual:response.statusCode, expected:testData.expectedStatus, test:'response status'},
+                    {assertion: testData.tests.bodyAssertion, actual:body, expected: testData.expectedBody, test: 'response body'}
+                ];
 
-                err ? testFunction.handleError(err, done) : testData.tests.after(null, testData, done);
+                //do the test if there is no error from the request
+                err ? testFunction.handleError(err, done) : testFunction.doAssertions(testData);
+
+                //do the test clean up actions
+                testData.tests.after(null, testData, done);
 
             });
 
