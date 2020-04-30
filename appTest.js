05768759cf59b0c1ec2933043c5bd9184c758b67
whitep <bose.lane@btinternet.com>
2020-04-30 09:33:54 +0100

    Refactor to make the code more readable.

--- appTest.js
+++ appTest.js
@@ -1,12 +1,31 @@
+/*!
+ * appTest
+ *
+ * Copyright(c) 2020 Peter Whitehead
+ * Apache Licensed
+ */
+
 'use strict';
 
-let request = require('request');
+/*
+Simple test application for GET requests supported in the express-api app.
+Does not support POST/UPDATE/DELETE
+ */
 
+let request = require('request');
+//Get input from the command line
 let myArgs = process.argv.slice(2);
 
+//The delay between request submissions
 let delay = myArgs[0] ? myArgs[0] : 0;
+
+//The ip Address of the application
 let ipAddress = myArgs[1] ?  myArgs[1] : '127.0.0.1:3000';
+
+//The API call to be made
 let apiCall = myArgs[2] ? myArgs[2] : status;
+
+//The request options
 let options = {
     url: 'http://'+ipAddress + "/api/"+apiCall,
     method: "get",
@@ -17,7 +36,6 @@ let options = {
 const testAlreadyRunning = 'Test is already running';
 
 console.log('Options: '+JSON.stringify(options));
-
 console.log('Delay = '+delay);
 console.log('ipAddress = '+ipAddress);
 
@@ -26,23 +44,36 @@ let start = Date.now();
 
 console.log('start time: '+start);
 
+//Send the initial request
 request(options, test);
 
-function test(err, res, body){
+/**
+ * test
+ *
+ * A recursive function run periodically.
+ * Send an http request and on completion print out the response body.
+ * Ignore messages stating that there has been a clash between api/test requests.
+ * Log any other errors.
+ *
+ * @param err
+ * @param res
+ */
+function test(err, res){
 
-    if (err){
-        console.log(increment+': ERROR: '+err);
+    let LogHeader = increment + ': delay: ' + delay + ': duration: '+ String(Date.now() - start - delay) + 'ms : ';
 
-    } else{
+    (err) ?
 
-        if ((res !== undefined) && (res.statusCode !== undefined ) && (body.err !== testAlreadyRunning)) {
+        console.log(increment+': ERROR: '+err)
+        //if the objects we need are defined
+        : ((res.statusCode !== undefined) && (res.body.err !== testAlreadyRunning)) ?
 
+            //if there is a good status log the full response otherwise output the response and the associated error code
             res.statusCode === 200 ?
-                console.log(increment + ': delay: ' + delay + ': duration: ' + String(Date.now() - start - delay) + 'ms : ' + JSON.stringify(body)) :
-                console.log(increment + ': ' + "ERROR: status code " + res.statusCode + ' ' + JSON.stringify(res.body));
-       }
+                console.log(LogHeader + JSON.stringify(res.body)) :
+                console.log(LogHeader + "ERROR: " + res.statusCode + ' ' + JSON.stringify(res.body))
 
-    }
+            : console.log('required objects are undefined ');
 
     increment++;
 
@@ -51,5 +82,3 @@ function test(err, res, body){
     setTimeout(()=>{request(options, test)},delay);
 
 }
-
-
