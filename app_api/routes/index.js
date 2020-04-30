69468ce7fa5401925b8f25726c7cc609c49c598a
whitep <bose.lane@btinternet.com>
2020-04-30 17:54:39 +0100

    Added source code file headers and did a major update of the README.md

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,14 +1,14 @@
 /**
-* index.js
-*
-* Created by Peter Whitehead March 2020
-*
-* Baseline application that serves an API and basic web pages
-*
-* Copyright Peter Whitehead @2020
-*
-* Licensed under Apache-2.0
-*/
+ * index.js
+ *
+ * Created by Peter Whitehead March 2020
+ *
+ * Baseline application that serves an API and basic web pages
+ *
+ * Copyright Peter Whitehead @2020
+ *
+ * Licensed under Apache-2.0
+ */
 
 'use strict';
 
