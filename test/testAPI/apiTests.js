e029d0ab354e4964ff2e1b864e0e676cdd904e39
whitep <bose.lane@btinternet.com>
2020-04-30 18:28:03 +0100

    Refactored the tests to make it simpler to read.

--- test/testAPI/apiTests.js
+++ test/testAPI/apiTests.js
@@ -17,14 +17,9 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 //Get the test definitions and the test functions
 const testData = require('./testData');
 const testFunctions = require('../utilities/testFunctions');
+const logger = require('../../app_utilities/logger');
 
-!testData ? logger._error({filename: __filename, methodname: 'main', nessage: 'test data is missing'}): null;
-
-//iterate through the array of test definitions: Assumes a JSON array object of test definitions
-//TODO: put in type checking and error handling for case where json object is not formed correctly.
-
-for(let index = 0; index < testData.testDefinitions.apiTests.length; index++){
-
-    testFunctions.testFunction(testData.testDefinitions.apiTests[index]);
-
-}
+//if there is valid test data iterate through the tests defined in testData
+!testData ?
+    logger._error({filename: __filename, methodname: 'main', nessage: 'test data is missing'})
+    : testData.testDefinitions.apiTests.forEach((value)=>{testFunctions.testFunction(value);});
