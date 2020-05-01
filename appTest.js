fb472cb8d5855f4e2bfa69ebdc767efc41c92881
whitep <bose.lane@btinternet.com>
2020-05-01 13:16:00 +0100

    Added a random key word that will cause the test to chose which API calls to make from a defined list.
    Add a flag so that error responses due to tests already running can be filtered out.

--- appTest.js
+++ appTest.js
@@ -10,6 +10,13 @@
 /*
 Simple test application for GET requests supported in the express-api app.
 Does not support POST/UPDATE/DELETE
+
+usage appTest.js <delay> <ipaddress:port> <test> <ignore testAlreadyRunning>
+
+<test> can be one of info, status, version, config, test, random
+<ingore testAlreadyRunning> can be true or false
+random will pick a new API command for each request based on a simple random number choice.
+
  */
 
 let request = require('request');
@@ -25,7 +32,11 @@ let ipAddress = myArgs[1] ?  myArgs[1] : '127.0.0.1:3000';
 //The API call to be made
 let apiCall = myArgs[2] ? myArgs[2] : status;
 
-//The request options
+//ignore test running
+
+let ingoreTestRunningError = myArgs[3] === 'true';
+
+//The request options used when a specific test is chosen
 let options = {
     url: 'http://'+ipAddress + "/api/"+apiCall,
     method: "get",
@@ -33,16 +44,55 @@ let options = {
     qs: {}
 };
 
+//Options used when random testing is chose
+let testOptions = [
+    {
+        url: 'http://'+ipAddress + '/api/info',
+        method: "get",
+        json: {},
+        qs: {}
+    },
+    {
+        url: 'http://'+ipAddress + '/api/status',
+        method: "get",
+        json: {},
+        qs: {}
+    },
+    {
+        url: 'http://'+ipAddress + '/api/version',
+        method: "get",
+        json: {},
+        qs: {}
+    },
+    {
+        url: 'http://'+ipAddress + '/api/config',
+        method: "get",
+        json: {},
+        qs: {}
+    },
+    {
+        url: 'http://'+ipAddress + '/api/test',
+        method: "get",
+        json: {},
+        qs: {}
+    }
+
+];
+
 const testAlreadyRunning = 'Test is already running';
 
 console.log('Options: '+JSON.stringify(options));
 console.log('Delay = '+delay);
 console.log('ipAddress = '+ipAddress);
+console.log('API: '+apiCall);
 
 let increment = 1;
 let start = Date.now();
 
 console.log('start time: '+start);
+
+//chose the correct options for the request
+options = apiCall === 'random' ? getRandomTest() : options;
 
 //Send the initial request
 request(options, test);
@@ -66,6 +116,9 @@ function test(err, res){
 
     start = Date.now();
 
+    options = apiCall === 'random' ? getRandomTest() : options;
+
+    //send a client request to the API server after a delay
     setTimeout(()=>{request(options, test)},delay);
 
 }
@@ -82,20 +135,69 @@ function handleErr(err){
 
 }
 
+/**
+ * responseReport
+ * report on the outcome of the client request
+ * @param res
+ */
 function responseReport(res){
 
-    let LogHeader = increment + ': delay: ' + delay + ': duration: '+ String(Date.now() - start - delay) + 'ms : ';
+    let LogMessage = increment + ': delay: ' + delay + ': duration: '+ String(Date.now() - start - delay) + 'ms : ';
 
     //validate that the data needed in the report is available
     //Do not report on errors that are due to a test already running if the API call is api/test
-    ((res.statusCode !== undefined) && ((res.body.err !== testAlreadyRunning) && apiCall === 'test')) ?
+    (res.statusCode !== undefined)  ?
 
         //if there is a good status log the full response otherwise log the response and the associated error code
         res.statusCode === 200 ?
+            logResults(LogMessage + JSON.stringify(res.body))
+            : filterReporting(res,LogMessage + "ERROR: " + res.statusCode + ' ' + JSON.stringify(res.body))
 
-            console.log(LogHeader + JSON.stringify(res.body)) :
-            console.log(LogHeader + "ERROR: " + res.statusCode + ' ' + JSON.stringify(res.body))
+        : console.log('ERROR: did not get a status from the client request');
 
-        : {};
+}
+
+/**
+ * logResults
+ *
+ * send the message to the required output medium.
+ * @param message
+ */
+function logResults(message){
+    console.log(message);
+}
+
+/**
+ * getRandomInt
+ * @param max
+ * @returns {number}
+ */
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+/**
+ * getRandomTest
+ * @returns {{qs: {}, method: string, json: {}, url: string}|{qs: {}, method: string, json: {}, url: string}|{qs: {}, method: string, json: {}, url: string}|{qs: {}, method: string, json: {}, url: string}|{qs: {}, method: string, json: {}, url: string}}
+ */
+function getRandomTest(){
+
+    return testOptions[getRandomInt(testOptions.length)];
+
+}
+
+/**
+ * filterReporting
+ * @param res
+ * @param message
+ */
+function filterReporting(res, message){
+
+    //we can choose to ignore logging errors that are caused by responses failing because the api/test is running.
+    //either log all failures or filter out those with a the defined err message.
+    ingoreTestRunningError === true ?
+        (res.body.err !== testAlreadyRunning) ?
+            logResults(message) : {}
+        : logResults(message);
 
 }
